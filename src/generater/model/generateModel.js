9144a503b2b70aa5ded8b58e39a2f98d620de737
yy1407 <134270007+yy1407@users.noreply.github.com>
2025-11-30 22:58:14 +0900

    fix castsのカンマ位置

--- src/generater/model/generateModel.js
+++ src/generater/model/generateModel.js
@@ -48,7 +48,7 @@ export const generateModelClassAttributesTemplate = (columns) => {
 
     // cast テンプレート
     if (castType) {
-      casts.push(`'${columnName}' => '${castType},'`);
+      casts.push(`'${columnName}' => '${castType}',`);
     }
 
     // fillable テンプレート
